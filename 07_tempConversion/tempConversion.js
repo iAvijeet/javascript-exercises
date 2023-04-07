579d6f540d7421cf478b458524374a255cbf4b87
avijeet <avijeetjaiswal112@gmail.com>
2023-04-07 23:20:34 +0530

    pass test tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  const Celsius = (Fahrenheit - 32) * 5/9;
+  return Math.round(Celsius * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celsius) {
+  const Fahrenheit = Celsius * 9/5 + 32;
+  return Math.round(Fahrenheit * 10) /10;
 };
 
 // Do not edit below this line
