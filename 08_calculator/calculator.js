06031cee779acf004851394de8b91e1ae2afa614
avijeet <avijeetjaiswal112@gmail.com>
2023-04-08 00:13:21 +0530

    pass test calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
+const add = function(x, y) {
+  return x + y;
 	
 };
 
-const subtract = function() {
+const subtract = function(x, y) {
+  return x - y;
 	
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.length
+    ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+    : 0;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  if (base === 0) {return 1;}
+  let result = 1;
+  for (i = 0; i < exponent; i++) {
+    result *= base;
+  }
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+	return result;
 };
 
 // Do not edit below this line
