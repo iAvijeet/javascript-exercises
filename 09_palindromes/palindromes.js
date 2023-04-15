962c35a9b09664d6145938cf7844e2645c467144
avijeet <avijeetjaiswal112@gmail.com>
2023-04-15 23:26:44 +0530

    pass test palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const cleanStr = str.toLowerCase().replace(/[^a-z]/g, ""); // Remove all non-alphanumeric characters and convert to lower case.
+  
+    return cleanStr.split("").reverse().join("") == cleanStr;
 
-};
-
+}
 // Do not edit below this line
 module.exports = palindromes;
